8d8d702d9bf9b641506d837eb66e339146df8760
Ariel <ariel.nevzglyad.hueso@gmail.com>
2024-02-25 16:35:45 +0100

    add more hours

--- .netlify/functions-internal/ssr/chunks/generic_Tb012mrE.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./pages/generic_BNu95GG4.mjs');
-
-export { page };
--- .netlify/functions-internal/ssr/chunks/index_xBA7JNFP.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./pages/index_D40c1h0U.mjs');
-
-export { page };
